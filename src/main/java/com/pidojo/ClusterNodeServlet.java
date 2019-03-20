b9d1f9310029a8ea88084369e93d820aa8d631c2
LightenWang <lightenwang@gmail.com>
2019-03-21 00:26:55 +0800

    update init() and destroy() methods for ClusterNodeServlet class

--- src/main/java/com/pidojo/ClusterNodeServlet.java
+++ src/main/java/com/pidojo/ClusterNodeServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.websocket.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @ClientEndpoint
 public class ClusterNodeServlet extends HttpServlet {
@@ -16,12 +18,23 @@ public class ClusterNodeServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        super.init();
+        this.nodeId = this.getInitParameter("nodeId");
+        String path = this.getServletContext().getContextPath() + "/clusterNodeSocket" + this.nodeId;
+        try {
+            URI uri = new URI("ws", "localhost:8080", path, null, null);
+            this.session = ContainerProvider.getWebSocketContainer().connectToServer(this, uri);
+        } catch (URISyntaxException | IOException | DeploymentException e) {
+            throw new ServletException("Cannot connect to" + path + ".", e);
+        }
     }
 
     @Override
     public void destroy() {
-        super.destroy();
+        try {
+            this.session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
